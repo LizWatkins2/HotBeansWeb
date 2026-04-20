c04fd9e642e74879eefc19848228b09ac1e5877f
Elisabeth Watkins <COMP5287746@gccomp.local>
2026-04-20 14:08:56 +0100

    first commit

--- Unit6Assignment2 HTML/script.js
+++ Unit6Assignment2 HTML/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function checkname() {
     let fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
@@ -19,4 +20,27 @@ function checkname() {
 
 function resetForm() {
     document.getElementById("Apply").reset();
+=======
+function checkname() {
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let phonenum = document.getElementById("phonenum").value;
+    let qualifications= document.getElementById("qualifications").value;
+    let personalwebsite = document.getElementById("personalwebsite").value;
+    let languages = document.getElementById("languages").value;
+
+
+    if (fname == "" || lname == "" || email == "" || phonenum == "" || qualifications == "" || personalwebsite == "" || languages == "") {
+        alert("Please ensure all fields are filled out correctly.")
+    }
+    else {
+        alert("Thank you" + " " + fname + " " + lname + " " + "your application was successful, we'll get back to you soon!");
+    }
+
+}
+
+function resetForm() {
+    document.getElementById("Apply").reset();
+>>>>>>> 922d5ca (first commit)
 }
