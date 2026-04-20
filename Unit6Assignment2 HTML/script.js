ee75572cac5a091c63c429ff07bd639d9c889517
LizWatkins2 <lizwatkins39@gmail.com>
2026-04-20 13:52:32 +0100

    Add files via upload

---
+++ Unit6Assignment2 HTML/script.js
@@ -0,0 +1,22 @@
+function checkname() {
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let phonenum = document.getElementById("phonenum").value;
+    let qualifications= document.getElementById("qualifications").value;
+    let personalwebsite = document.getElementById("personalwebsite").value;
+    let languages = document.getElementById("languages").value;
+
+
+    if (fname == "" || lname == "" || email == "" || phonenum == "" || qualifications == "" || personalwebsite == "" || languages == "") {
+        alert("Please ensure all fields are filled out correctly.")
+    }
+    else {
+        alert("Thank you" + " " + fname + " " + lname + " " + "your application was successful, we'll get back to you soon!");
+    }
+
+}
+
+function resetForm() {
+    document.getElementById("Apply").reset();
+}
